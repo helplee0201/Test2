a874149dd52564c02f9f81a988d6fe5c21b8e4a3
helplee0201 <de.daily2023@gmail.com>
2025-09-03 12:06:19 +0900

    Update web.py

--- web.py
+++ web.py
@@ -174,8 +174,8 @@ insured_dict = {entry['no_biz']: entry['nm_krcom'] for entry in PARSED_DATA}
 contractor_dict = {entry['no_bisocial']: entry['nm_trade'] for entry in PARSED_DATA}
 
 # Sorted options for selectboxes
-insured_options = sorted([f"{num} - {name} (판매자)" for num, name in insured_dict.items()])
-contractor_options = sorted([f"{num} - {name} (구매자)" for num, name in contractor_dict.items()])
+insured_options = sorted([f"{num} - {name} (피보험자)" for num, name in insured_dict.items()])
+contractor_options = sorted([f"{num} - {name} (계약자)" for num, name in contractor_dict.items()])
 
 # Set page config for wide layout
 st.set_page_config(layout="wide")
@@ -594,6 +594,7 @@ if st.session_state.network_run and st.session_state.htmls:
                     st.markdown('<p class="no-fraud">사기거래 징후가 보이지 않습니다.</p>', unsafe_allow_html=True)
             else:
                 st.info("‘매출매입 상세’ 또는 ‘사기거래 분석’을 클릭하여 상세 정보를 확인하세요.")
+
 
 
 
